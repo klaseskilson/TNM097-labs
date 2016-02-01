8285d34bdab2d2cde2a51c79ddd8bd84b3289477
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-01 14:03:46 +0100

    Calculate device-dependent rgb data

--- lab2/main.m
+++ lab2/main.m
@@ -8,4 +8,10 @@ load('illum.mat', 'CIED65');
 illum = repmat(CIED65, size(chips20, 1), 1);
 rgb_raw = (illum .* chips20) * Ad;
 
-%% 2.1
+%% 2.1 Compute normalization factors
+e = ones(1, 61);
+norm_fact = (CIED65 .* e) * Ad;
+
+%% 2.2 Compute calibrated device-dependent RGB data
+big_norm_fact = repmat(norm_fact, size(chips20, 1), 1);
+rgb_cal = rgb_raw ./ big_norm_fact;
