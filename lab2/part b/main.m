7c097df6122a623961f426b8b7c971799d587379
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-08 11:03:01 +0100

    Add 6.2 plots

--- lab2/part b/main.m
+++ lab2/part b/main.m
@@ -10,12 +10,20 @@ plot(Spec_300(20,:), 'gx')
 plot(Spec_600(20,:), 'g--')
 
 % 50%!
-plot(Spec_150(20,:)*An(13) + (1-An(13)) * Spec_150(1,:), 'r--')
-plot(Spec_300(20,:)*An(13) + (1-An(13)) * Spec_300(1,:), 'm--')
-plot(Spec_600(20,:)*An(13) + (1-An(13)) * Spec_600(1,:), 'k--')
+plot(Spec_150(20,:) * An(13) + (1 - An(13)) * Spec_150(1,:), 'r--')
+plot(Spec_300(20,:) * An(13) + (1 - An(13)) * Spec_300(1,:), 'm--')
+plot(Spec_600(20,:) * An(13) + (1 - An(13)) * Spec_600(1,:), 'k--')
 plot(Spec_150(13,:), 'rx')
 plot(Spec_300(13,:), 'mx')
 plot(Spec_600(13,:), 'kx')
 
 %% 6.2 Effective ink coverage
-
+figure;
+hold on;
+a_eff = zeros(size(Spec_150));
+for i = 1:size(Spec_150, 1);
+    a_eff(i,:) = (Spec_150(i,:) - Spec_150(1,:)) ./ (Spec_150(20,:) - Spec_150(1,:));
+    legendInfo{i} = ['X = ' num2str(An(i) * 100) '%'];
+end;
+plot(a_eff');
+legend(legendInfo);
