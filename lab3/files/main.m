88985c0785f76ce7825fbdb4ff6c549314eb1ed3
Johan Reimann <johre099@student.liu.se>
2016-02-12 10:36:13 +0100

    Completed task 2.1.1 with noise measurements

--- lab3/files/main.m
+++ lab3/files/main.m
@@ -12,3 +12,26 @@ plot_chrom(XYZdlp', 'r');
 hold on;
 plot_chrom(XYZdell, 'g');
 plot_chrom(XYZinkjet, 'b');
+
+%% 2.1.1 Mathematical metrics - grayscale image - interpolation
+
+p = im2double(imread('peppers_gray.tif'));
+p_nearest = imresize(imresize(p,0.25,'nearest'),4,'nearest');
+p_bilinear = imresize(imresize(p,0.25,'bilinear'),4,'bilinear');
+p_bicubic = imresize(imresize(p,0.25,'bicubic'),4,'bicubic');
+
+MSE_nearest = mean(mean((p - p_nearest).^2));
+MSE_bilinear = mean(mean((p - p_bilinear).^2));
+MSE_bicubic = mean(mean((p - p_bicubic).^2));
+
+SNR_nearest = snr(p, p - p_nearest);
+SNR_bilinear = snr(p, p - p_bilinear);
+SNR_bicubic = snr(p, p - p_bicubic);
+
+subplot(2,2,1), imshow(p)
+subplot(2,2,2), imshow(p_nearest)
+subplot(2,2,3), imshow(p_bilinear)
+subplot(2,2,4), imshow(p_bicubic)
+
+%% 2.1.2 Grayscale image halftoning
+
